4aaf20d7365784abafc9e4c783c2891ad6778ada
sakshamengineer <sakshamkgupta01@gmail.com>
2025-03-13 20:51:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let btn = document.querySelector('.access');
-cs = document.querySelectorAll('.access1');
-cc = document.querySelector('.access2');
-c = document.querySelector('.access3');
-img = document.querySelectorAll("img");
+let cs = document.querySelectorAll('.access1');
+let cc = document.querySelector('.access2');
+let c = document.querySelector('.access3');
+let img = document.querySelectorAll("img");
 btn.addEventListener('click',()=>{
     cs.forEach((element) => {
         element.classList.toggle("cube-side");
@@ -13,4 +13,10 @@ btn.addEventListener('click',()=>{
     cc.classList.toggle("cube-container");
     cc.classList.toggle("cube-container-1");
     c.classList.toggle("cube");
+    if (btn.innerText === "Expand the box"){
+        btn.innerText = "Shrink to cube";
+    }
+    else if (btn.innerText === "Shrink to cube"){
+        btn.innerText = "Expand the box";
+    }
 })
